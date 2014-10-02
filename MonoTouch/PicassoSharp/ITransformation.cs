1c8d104a298ca71a29996260f24f1ddb4cc3224e
Karl Stewart <sqvarehead@gmail.com>
2014-10-02 16:20:41 -0400

    Added ITransformation interface to iOS

---
+++ MonoTouch/PicassoSharp/ITransformation.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Exception = System.Exception;
+
+namespace PicassoSharp
+{
+	public interface ITransformation
+	{
+		String Key
+		{
+			get;
+		}
+	}
+
+}
+
